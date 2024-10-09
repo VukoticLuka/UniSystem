fac9d2c0262b7abd76761c30d871093fcefa6d52
luka_120_2019 <lukavukotic811@gmail..com>
2024-10-09 14:33:22 +0200

    Student schemas columns are changed

--- app/models/schemas/student_schema.py
+++ app/models/schemas/student_schema.py
@@ -1,7 +1,9 @@
-from typing import Literal, Union
+from typing import Literal, Union, Optional
 
 from pydantic import BaseModel, Field, EmailStr, AliasGenerator, ConfigDict, field_validator, ValidationError
-from ..student_model import Level
+from ..student import Level, Gender
+
+from datetime import datetime
 
 
 class DbStudent(BaseModel):
@@ -9,12 +11,22 @@ class DbStudent(BaseModel):
     email: EmailStr = Field(default="user@example.com")
     firstname: str
     lastname: str
-    level: Level = Field(default=Level.BACHELOR.value)
+    index_number: int = Field(ge=1)
+    enrollment_year: int
+    gender: Optional[Gender] = Field(default=None)
+    education: Optional[Level] = Field(default=Level.BACHELOR.value)
 
-    @field_validator("level")
+    @field_validator("education")
     def check_level(cls, value):
         if value not in Level.__members__.values():
             raise ValueError("Level must be: bachelor, master or doctoral")
+        return value
+
+    @field_validator("enrollment_year")
+    def check_enrollment_year(cls, value):
+        curr_year = datetime.now().year
+        if value > curr_year or value < curr_year - 10:
+            raise ValueError(f"Enrollment year mus be between {curr_year - 10} and {curr_year}")
         return value
 
 
@@ -27,11 +39,10 @@ class StudentDisplay(DbStudent):
 
 
 class StudentUpdate(BaseModel):
-    email: Union[EmailStr | None] = Field(default=None)
-    firstname: str | None = None
-    lastname: str | None = None
-    level: Union[Level | None] = Field(default=None)
-
-
-
-
+    email: Union[EmailStr, None] = Field(default=None)
+    firstname: Union[str, None] = Field(default=None)
+    lastname: Union[str, None] = Field(default=None)
+    index_number: Union[int, None] = Field(default=None)
+    enrollment_year: Union[int, None] = Field(default=None)
+    gender: Union[Gender, None] = Field(default=None)
+    education: Union[Level, None] = Field(default=None)
