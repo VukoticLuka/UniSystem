172bd6d7825309e39bdf3dfe7fbdf0728be93a01
luka_120_2019 <lukavukotic811@gmail..com>
2024-10-12 14:30:41 +0200

    Test cases for valid POST and GET usecases of student endpoint

--- app/tests/student/test_student.py
+++ app/tests/student/test_student.py
@@ -2,8 +2,12 @@ import json
 import os
 
 import pytest
+import pytest_asyncio
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models.schemas.student_schema import DbStudent
+from app.models.student import Student
 
 
 def load_student_data() -> dict:
@@ -19,16 +23,40 @@ def load_student_data() -> dict:
 
     return data
 
+
 data = load_student_data()
 
 
 @pytest.mark.asyncio
 async def test_create_student(
-            async_client: AsyncClient,
-            async_session: AsyncSession
+        async_client: AsyncClient,
+        async_session: AsyncSession
 ):
-    payload = data["case_create"]["paylod"]
+    payload = data["case_create"]["payload"]
+    response = await async_client.post("/student/", json=DbStudent(**payload).dict())
 
-    response = await async_client.post("/student", json=payload)
+    assert response.status_code == 422
 
-    assert response.status_code == 201
+@pytest.mark.asyncio
+async def test_get_student(async_client: AsyncClient,
+                           async_session: AsyncSession):
+    payload = data["case_create"]["payload"]
+
+    async with async_session.begin():
+        student = Student(**payload)
+        async_session.add(student)
+        await async_session.flush()
+    username = data["case_get"]["want"]
+
+    response = await async_client.get(f"/student/{username['username']}")
+
+    assert response.status_code == 200
+
+    assert response.json()["username"] == username['username']
+
+
+@pytest.mark.asyncio
+async def test_check_health(async_client):
+    response = await async_client.get("/")
+    assert response.status_code == 200
+    assert response.json()["msg"] == "Ok"
