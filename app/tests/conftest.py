e2f036f9da2ea0c1ac43a373ee04d6462cc3e98d
luka_120_2019 <lukavukotic811@gmail..com>
2024-10-12 12:11:16 +0200

    Implemented pytest fixtures in conftest file

---
+++ app/tests/conftest.py
@@ -0,0 +1,45 @@
+import asyncio
+from typing import Generator
+
+import pytest
+import pytest_asyncio
+
+import httpx
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.base import Base
+from app.main import app
+
+
+@pytest.fixture(scope="session")
+def event_loop(request) -> Generator:
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture
+async def async_client():
+    async with httpx.AsyncClient(
+            app=app,
+            base_url="http://testserver"
+    ) as client:
+        yield client
+
+@pytest_asyncio.fixture(scope="function")
+async def async_session() -> AsyncSession:
+    from sqlalchemy.orm import sessionmaker
+    from app.core.session import async_engine
+    session = sessionmaker(
+        async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with session() as s:
+        async with async_engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+        yield s
+
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+    await async_engine.dispose()
