6d3a8c01f5904c08ec410a83e8ee49ba64820c65
luka_120_2019 <lukavukotic811@gmail..com>
2024-10-14 16:15:20 +0200

    Added grade column to student_course table

--- app/models/stud_course.py
+++ app/models/stud_course.py
@@ -8,7 +8,6 @@ student_course = Table(
     "student_course",
     Base.metadata,
     Column("course_id", Integer, ForeignKey("courses.course_id"), primary_key=True),
-    Column("student_id", Integer, ForeignKey("students.id"), primary_key=True)
+    Column("student_id", Integer, ForeignKey("students.id"), primary_key=True),
+    Column("grade", Integer, nullable=True)
 )
-
-
